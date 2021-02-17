39c3795ce99985d6d68898ed84478bc8167030c6
Yc <STM32_Yc@sjtu.edu.cn>
2021-02-17 23:49:14 +0800

    在x轴正半轴发射到原点时半圆轨迹消失

---
+++ Assets/Scripts/Build/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildManager : MonoBehaviour
+{
+    public GameObject turretPrefab;
+
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //turretPrefab = Resources.Load<GameObject>("Prefabs/Turret");
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //if (EventSystem.current.IsPointerOverGameObject() == false)
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool isCollider = Physics.Raycast(ray, out hit, 1000);
+            if (isCollider)
+            {
+                Instantiate(turretPrefab, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
--- Assets/Scripts/Missile/Missile.cs
+++ Assets/Scripts/Missile/Missile.cs
@@ -13,18 +13,16 @@ public class Missile : MonoBehaviour
 
     private float circleTime;
     private bool circleFlag;
+    private Vector3 worldUp;
+
+    private float verticalHeight;
+    private float speed;
+    private readonly float maxSpeed = 7;
 
     private void Awake()
     {
         //transform.position = new Vector3(0, 0, 0);
-        //startPos = transform.position;
-        //endPos = new Vector3(10, 0, 0);
-        pointsList = new List<Vector3>
-        {
-            new Vector3(0, 0, 0), new Vector3(0, 10, 0), new Vector3(10, 10, 0), new Vector3(10, 0, 0)
-        };
-
-        transform.position = pointsList[0];
+        pointsList = new List<Vector3>();
         state = 0;
         circleFlag = true;
     }
@@ -32,13 +30,14 @@ public class Missile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = maxSpeed;
+        verticalHeight = 10f;
+        SetTarget(new Vector3(0, 0, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        //CircleOrbit(pointsList[1],pointsList[2]);
         Fly();
     }
 
@@ -59,40 +58,68 @@ public class Missile : MonoBehaviour
         Vector3 center = (start + end) / 2;
         float radius = Vector3.Distance(center, start);
         float fai = Mathf.Atan((center.z - start.z) / (center.x - start.x));
-        float x = center.x - radius * Mathf.Cos((Time.time - circleTime) / 2) * Mathf.Cos(fai);
-        float y = center.y + radius * Mathf.Sin((Time.time - circleTime) / 2);
-        float z = center.z - radius * Mathf.Cos((Time.time - circleTime) / 2) * Mathf.Sin(fai);
-        print(x);
+        print(fai / Mathf.PI * 180f);
+        float x = center.x - radius * Mathf.Cos((Time.time - circleTime) * (speed / radius * 8)) * Mathf.Cos(fai);
+        float y = center.y + radius * Mathf.Sin((Time.time - circleTime) * (speed / radius * 8));
+        float z = center.z - radius * Mathf.Cos((Time.time - circleTime) * (speed / radius * 8)) * Mathf.Sin(fai);
         transform.position = new Vector3(x, y, z);
-        transform.LookAt(center, Vector3.right);
+        transform.LookAt(center, worldUp);
     }
 
     private void LinearOrbit(Vector3 start, Vector3 end)
     {
-        transform.Translate((end - start) * Time.deltaTime / 3, Space.World);
+        transform.Translate((end - start) * (Time.deltaTime * speed), Space.World);
     }
 
     private void StateSwitch()
     {
-        if (Vector3.Distance(transform.position, pointsList[0]) < 0.01)
+        //print(transform.position);
+        if (Vector3.Distance(transform.position, pointsList[0]) < 0.1)
         {
             state = 0;
         }
-        if (Vector3.Distance(transform.position, pointsList[1]) < 0.01) //Math.Abs(transform.position.y - pointsList[1].y) < 0.01
-        //transform.position.y - pointsList[1].y > 0.1 && transform.position.y - pointsList[1].y < 0.2
+        
+        if (Vector3.Distance(transform.position, pointsList[1]) < 0.5)
         {
             state = 1;
             if(circleFlag)
                 circleTime = Time.time;
             circleFlag = false;
         }
-        if (Vector3.Distance(transform.position, pointsList[2]) < 0.03)
+        else if (Vector3.Distance(transform.position, pointsList[1]) < 1)
+        {
+            //speed = 1;
+        }
+        else
+        {
+            //speed = maxSpeed;
+        }
+        
+        if (Vector3.Distance(transform.position, pointsList[2]) < 0.5)
         {
             state = 2;
         }
-        if (Vector3.Distance(transform.position, pointsList[3]) < 1)
+        if (Vector3.Distance(transform.position, pointsList[3]) < 0.5)
         {
             state = -1;
+            Destroy(gameObject);
         }
+    }
+
+    void SetTarget(Vector3 targetPos)
+    {
+        var position = transform.position;
+        if (targetPos.y - position.y > verticalHeight)
+        {
+            Debug.LogError("The target is too high.");
+        }
+        pointsList = new List<Vector3>
+        {
+            position,
+            new Vector3(position.x, position.y + verticalHeight, position.z),
+            new Vector3(targetPos.x, position.y + verticalHeight, targetPos.z),
+            targetPos
+        };
+        worldUp = Vector3.Normalize(new Vector3(pointsList[3].x - pointsList[0].x, 0, pointsList[3].z - pointsList[0].z));
     }
 }
